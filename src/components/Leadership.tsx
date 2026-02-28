80460d2ec3f29e59315051c72a4ca7113457dd9a
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-02-28 12:08:30 +0000

    main backrgound added

--- src/components/Leadership.tsx
+++ src/components/Leadership.tsx
@@ -95,7 +95,7 @@ export function Leadership() {
                 />
 
                 <div className="relative z-10">
-                  {/* Profile Image */}
+                  {/* Profile Image added*/}
                   <motion.div
                     className="w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 mx-auto mb-4 sm:mb-6 relative"
                     whileHover={{ scale: 1.05 }}
