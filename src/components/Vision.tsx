3d95224fd075f166d7b3f9303444691de24d92d6
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-04 10:03:56 +0000

    new feature added

--- src/components/Vision.tsx
+++ src/components/Vision.tsx
@@ -96,7 +96,7 @@ export function Vision() {
           </div>
         </motion.div>
 
-        {/* Principles */}
+        {/* Principles added*/}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 sm:gap-6">
           {principles.map((principle, index) => (
             <motion.div
