a9620ec6af86fa7b885f73ef13edc1524e01e667
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-04 10:04:22 +0000

    new feature added

--- src/components/Vision.tsx
+++ src/components/Vision.tsx
@@ -125,7 +125,7 @@ export function Vision() {
           ))}
         </div>
 
-        {/* This is bigger than a community */}
+        {/* Forge is bigger than a community */}
         <motion.div
           initial={{ opacity: 0, y: 50 }}
           animate={isInView ? { opacity: 1, y: 0 } : {}}
